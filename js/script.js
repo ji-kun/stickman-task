ab9712247fa85532ee5039fbc94d3abfe396ce52
Rohit Kunji <srinivasan.kunji@gmail.com>
2019-09-27 09:38:14 +0530

    implement sidenav hiding

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-const sidebtns = document.querySelectorAll(".sntop > div");
-const maindivs = document.querySelectorAll(".main > div");
+const sideBtns = document.querySelectorAll(".sntop > div");
+const mainDivs = document.querySelectorAll(".main > div");
+const noNavBtn = document.getElementsByClassName("arrow-left")[0];
+const navBtn = document.getElementsByClassName("burgers")[0];
+const container = document.getElementsByClassName("container")[0];
 
 let previ = 0;
 
 const showDiv = (i) => {
-    maindivs[previ].classList.remove("active");
-    sidebtns[previ].classList.remove("active");
+    mainDivs[previ].classList.remove("active");
+    sideBtns[previ].classList.remove("active");
 
-    maindivs[i].classList.add("active");
-    sidebtns[i].classList.add("active");
+    mainDivs[i].classList.add("active");
+    sideBtns[i].classList.add("active");
     
     previ = i;
 }
 
-sidebtns.forEach((btn, i) => {
+const hideNav = () => {
+    container.classList.add("nonav");
+}
+const showNav = () => {
+    container.classList.remove("nonav");
+}
+
+sideBtns.forEach((btn, i) => {
     btn.addEventListener("click", (evt) => {showDiv(i)});
 });
+
+noNavBtn.addEventListener("click", hideNav);
+navBtn.addEventListener("click", showNav);
